9b0f391e62a71f0aab97924915899d03e6a22c87
anton <antonkuzmn1@gmail.com>
2025-12-31 16:25:02 +0700

    airplant init

---
+++ src/main/java/com/antonkuzmn1/airplant/AirPlant.java
@@ -0,0 +1,12 @@
+package com.antonkuzmn1.airplant;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(AirPlant.MOD_ID)
+public class AirPlant {
+    public static final String MOD_ID = "airplant";
+
+    public AirPlant(FMLJavaModLoadingContext context) {
+    }
+}
